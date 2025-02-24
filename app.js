f0f15185ffaf43138d64ddbf2a106878e79bb024
programming-dip <anudipbhoumik@gmail.com>
2025-02-24 19:10:15 +1100

    final commit

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ const parentElement = document.getElementById("parentElement");
 const showMassage = document.getElementById("showMassage");
 const changeColor = document.body.style;
 
-propose = () => {
+yes = () => {
   parentElement.style.display = "none";
   showMessage.style.display = "block";
   changeColor.background =
     "linear-gradient(116.82deg, #ff94e7 0%, #27cbff 100%)";
+};
+
+let attempts = 0;
+no = () => {
+            const responseMessage = document.getElementById('responseMessage');
+            const noButton = document.getElementById('noButton');
+            
+            if (attempts === 0) {
+              responseMessage.innerHTML = "Try again babe, you pressed the wrong answer.";
+          } else if (attempts === 1) {
+              responseMessage.innerHTML = "Anwesha, where's your glasses 👓? As you pressed the wrong one again. Let's try again.";
+          } else if (attempts === 2) {
+              responseMessage.innerHTML = "One more time babe... You need help finding the right answer. Here's a hint 🤔: Starts with 'y' and ends with 'es'.";
+          } else if (attempts === 3) {
+              responseMessage.innerHTML = "It seems you're struggling. The correct answer is 'Yes'☺️.";
+          }
+          attempts++;
+
 };
 
 // Animate Text with Anim JS
